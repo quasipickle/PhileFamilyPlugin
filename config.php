d5042ae65acaa56841c5b7dfac1e3bc125c78533
Dylan Anderson <dylananderson@Dylans-iMac.local>
2014-05-11 21:13:05 -0600

    Initial publish

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	# If TRUE, then directories that are siblings of pages will have their index file
+	# included in the list of siblings.
+	#
+	# If FALSE, only actual pages will be included
+	'sibling_dirs' => TRUE
+);
